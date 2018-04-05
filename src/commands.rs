de027491d7acf66d69ea0ed5b5b75cae63102d7a
Yiran Sheng <shengyiran@gmail.com>
2018-04-05 15:36:28 -0700

    Replace manual mem::replace with take

--- src/commands.rs
+++ src/commands.rs
@@ -23,13 +23,13 @@ impl Command {
                 if len == 0 {
                     return Err(ProtocolError::ParseError);
                 }
-                let keyword = mem::replace(&mut protos[0], RespProtocol::Null);
+                let keyword = protos[0].take();
                 let keyword = keyword.as_bytes()?;
 
                 match keyword {
                     b"GET" => {
                         if len == 2 {
-                            let key = mem::replace(&mut protos[1], RespProtocol::Null);
+                            let key = protos[1].take();
                             let key = key.try_into_string_value()?;
                             Ok(Command::GET { key })
                         } else {
@@ -38,9 +38,9 @@ impl Command {
                     }
                     b"SET" => {
                         if len == 3 {
-                            let key = mem::replace(&mut protos[1], RespProtocol::Null);
+                            let key = protos[1].take();
                             let key = key.try_into_string_value()?;
-                            let value = mem::replace(&mut protos[2], RespProtocol::Null);
+                            let value = protos[2].take();
                             let value = value.try_into_string_value()?;
                             Ok(Command::SET { key, value })
                         } else {
