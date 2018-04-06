5a3007bdde93d56fe2b377f7a15d4fcbc03345e0
Yiran Sheng <shengyiran@gmail.com>
2018-04-06 16:36:33 -0700

    Complete Decode from buffer

--- src/redis_value.rs
+++ src/redis_value.rs
@@ -13,6 +13,55 @@ pub enum Value<T> {
     BulkString(T),
     Nil,
 }
+impl<T> Value<T> {
+    fn map<R, F>(self, f: F) -> Value<R>
+    where
+        F: FnOnce(T) -> R,
+    {
+        match self {
+            Value::SimpleString(x) => Value::SimpleString(f(x)),
+            Value::ErrorString(x) => Value::ErrorString(f(x)),
+            Value::IntegerString(x) => Value::IntegerString(f(x)),
+            Value::BulkString(x) => Value::BulkString(f(x)),
+            Value::Nil => Value::Nil,
+        }
+    }
+
+    pub fn as_ref(&self) -> Value<&T> {
+        match self {
+            &Value::SimpleString(ref x) => Value::SimpleString(x),
+            &Value::ErrorString(ref x) => Value::ErrorString(x),
+            &Value::IntegerString(ref x) => Value::IntegerString(x),
+            &Value::BulkString(ref x) => Value::BulkString(x),
+            _ => Value::Nil,
+        }
+    }
+    pub fn into_option(self) -> Option<T> {
+        match self {
+            Value::SimpleString(x) => Some(x),
+            Value::ErrorString(x) => Some(x),
+            Value::IntegerString(x) => Some(x),
+            Value::BulkString(x) => Some(x),
+            Value::Nil => None,
+        }
+    }
+    pub fn iter(&self) -> ValueIter<T> {
+        ValueIter {
+            value: self.as_ref().into_option(),
+        }
+    }
+}
+
+pub struct ValueIter<'a, T: 'a> {
+    value: Option<&'a T>,
+}
+impl<'a, T> Iterator for ValueIter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.value.take()
+    }
+}
 
 impl<'a, T: Into<&'a [u8]>> Into<&'a [u8]> for Value<T> {
     fn into(self) -> &'a [u8] {
@@ -38,16 +87,76 @@ pub enum Node<T> {
     Open(usize),
     Close,
 }
+impl<T> Node<T> {
+    fn map<R, F>(self, f: F) -> Node<R>
+    where
+        F: FnOnce(T) -> R,
+    {
+        match self {
+            Node::Leaf(v) => Node::Leaf(v.map(f)),
+            Node::Open(n) => Node::Open(n),
+            Node::Close => Node::Close,
+        }
+    }
+    fn value_iter(&self) -> ValueIter<T> {
+        match self {
+            &Node::Leaf(ref x) => x.iter(),
+            _ => ValueIter { value: None },
+        }
+    }
+}
 
 #[derive(Debug)]
 pub struct RedisValue {
     nodes: Vec<Node<Bytes>>,
 }
 impl RedisValue {
-    #[inline]
-    fn from_value(v: Value<Bytes>) -> Self {
-        RedisValue {
-            nodes: vec![Node::Leaf(v)],
+    pub fn decode(buf: &mut BytesMut) -> Result<Option<Self>, ()> {
+        let result = decode_values_from_slice(buf.as_ref());
+        match result {
+            Err(DecodeError::Incomplete) => Ok(None),
+            Err(DecodeError::Failed) => Err(()),
+            Ok((_, values)) => {
+                let n_bytes = values.byte_count();
+                match values {
+                    Values::One(value) => {
+                        // copy from buffer
+                        let value = value.map(|r| Bytes::from(&buf[r]));
+                        Ok(Some(RedisValue {
+                            nodes: vec![Node::Leaf(value)],
+                        }))
+                    }
+                    Values::Many(mut nodes) => {
+                        // will copy bytes from input buf into this
+                        // Leaf nodes's Bytes will share memory in this
+                        // compat Bytes object with exactly enough bytes
+                        let mut bytes = Bytes::with_capacity(n_bytes);
+                        let nodes = nodes
+                            .drain(..)
+                            .map(|node| match node {
+                                Node::Leaf(value) => {
+                                    let rng = value.as_ref().into_option();
+                                    if let Some(rng) = rng {
+                                        let rng = rng.clone();
+                                        let start = bytes.len();
+                                        let end = start + (rng.end - rng.start);
+
+                                        bytes.extend_from_slice(&buf[rng]);
+                                        let leaf_bytes = bytes.slice(start, end);
+                                        // clone here does not allocate, I think..
+                                        Node::Leaf(value.as_ref().map(|_| leaf_bytes.clone()))
+                                    } else {
+                                        Node::Leaf(Value::Nil)
+                                    }
+                                }
+                                Node::Open(count) => Node::Open(count),
+                                Node::Close => Node::Close,
+                            })
+                            .collect();
+                        Ok(Some(RedisValue { nodes }))
+                    }
+                }
+            }
         }
     }
 }
@@ -56,6 +165,18 @@ impl RedisValue {
 enum Values {
     One(Value<Range>),
     Many(Vec<Node<Range>>),
+}
+impl Values {
+    fn byte_count(&self) -> usize {
+        match self {
+            &Values::One(ref v) => v.iter().map(|r| r.end - r.start).sum(),
+            &Values::Many(ref nodes) => nodes
+                .iter()
+                .flat_map(Node::value_iter)
+                .map(|r| r.end - r.start)
+                .sum(),
+        }
+    }
 }
 #[derive(Debug)]
 enum DecodeError {
@@ -196,7 +317,7 @@ mod tests {
         ];
         for raw in &ok_tests {
             let result = decode_values_from_slice(raw.as_bytes());
-            let (consumed, r) = result.unwrap();
+            let (consumed, _) = result.unwrap();
             assert_eq!(consumed, raw.as_bytes().len());
         }
     }
